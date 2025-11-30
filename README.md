4ad8a74ade3ab88b210a344c00a785e5a40de243
wenxig <wenxinguo12@gmail.com>
2025-11-30 22:37:59 +0800

    chore: add source

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 功能
 
-- 自动抓取[Alvin9999/new-pac](https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7)的v2ray节点
+- 自动抓取[Alvin9999/new-pac](https://github.com/Alvin9999/new-pac)的v2ray和ss节点
 - 保存到`./data/sub.txt`
 
 ## 使用方法
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,21 @@ import path from 'path'
 import { JSDOM } from "jsdom"
 async function fetchAndWrite() {
   // 1. 从网络获取内容
-  console.log('begin get')
-  const url = 'https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7'
-  const res = await fetch(url, {
-    method: 'GET',
-  })
-  console.log('header done')
-  if (!res.ok) {
-    throw new Error(`请求失败：${res.status} ${res.statusText}`)
-  }
-  const { document } = (new JSDOM(await res.text())).window
-  const text = Array.from(document.querySelectorAll<HTMLDivElement>('.highlight-source-shell')).map(v => v.textContent).join('\n')
-  console.log('body done', text)
-
+  const sources = [
+    'https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
+    'https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7'
+  ]
+  const nodeLists = await Promise.all(sources.map(async source => {
+    const res = await fetch(source, {
+      method: 'GET',
+    })
+    console.log('header done')
+    if (!res.ok) {
+      throw new Error(`请求失败：${res.status} ${res.statusText}`)
+    }
+    const { document } = (new JSDOM(await res.text())).window
+    return Array.from(document.querySelectorAll<HTMLDivElement>('.highlight-source-shell')).map(v => v.textContent)
+  }))
 
   // 2. 确保输出目录存在
   const outDir = path.resolve(process.cwd(), 'data')
@@ -23,7 +25,7 @@ async function fetchAndWrite() {
 
   // 3. 写入文件
   const outPath = path.join(outDir, 'sub.txt')
-  await writeFile(outPath, text, 'utf8')
+  await writeFile(outPath, nodeLists.flat().join('\n'), 'utf8')
 
   console.log(`✅ 已写入 ${outPath}`)
 }
