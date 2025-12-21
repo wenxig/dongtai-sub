85309245a069dc6f1274265b1f5ca62e38aaa759
wenxig <117057186+wenxig@users.noreply.github.com>
2025-12-21 20:21:11 +0800

    Add additional sources to fetchAndWrite function

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ async function fetchAndWrite() {
   // 1. 从网络获取内容
   const sources = [
     'https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
-    'https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7'
+    'https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
+    'https://github.com/Alvin9999-newpac/fanqiang/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
+    'https://github.com/Alvin9999-newpac/fanqiang/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
   ]
   const nodeLists = await Promise.all(sources.map(async source => {
     const res = await fetch(source, {
