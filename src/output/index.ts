1643b4ef0eac5c422778bf1a458beede838a3b8b
denny0810 <59128785+denny0810@users.noreply.github.com>
2026-02-13 16:32:25 +0800

    update index.ts

--- src/output/index.ts
+++ src/output/index.ts
@@ -2,6 +2,7 @@ import type { Generator } from '../model'
 
 import v2ray from './v2ray'
 import clash from './clash'
+import mihomo from './mihomo'
 
 export const outputResults: Generator = (subs, dir) =>
-  Promise.all([v2ray(subs, dir), clash(subs, dir)])
+  Promise.all([v2ray(subs, dir), clash(subs, dir), mihomo(subs, dir)])
